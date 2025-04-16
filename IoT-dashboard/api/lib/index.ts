5724032b8dd82bdf612b7381310d7ac1d99bbc10
Damian Ćwik <50170810+Damian-Cwik@users.noreply.github.com>
2025-04-16 23:56:39 +0200

    Lab07

--- IoT-dashboard/api/lib/index.ts
+++ IoT-dashboard/api/lib/index.ts
@@ -1,6 +1,24 @@
-import DataController from './controllers/DataController';
 import App from './app';
+import DataController from './controllers/DataController';
+import IndexController from "./controllers/index.controller";
+import ReadingController from './controllers/reading.controller';
+import { Server } from 'socket.io';
 
-const app: App = new App([new DataController()]);
+const app: App = new App([]);
+const io = app.getIo();
+
+
+const controllers = [
+   new DataController(),
+   new IndexController(io),
+   new ReadingController(io),
+];
+
+
+controllers.forEach((controller) => {
+   app.app.use("/", controller.router);
+});
+
 
 app.listen();
+
