023c7c28cb9b99fd4b9c282304fff9b718660eef
Damian Ćwik <50170810+Damian-Cwik@users.noreply.github.com>
2025-06-02 02:39:35 +0200

    Lab10

--- IoT-dashboard - projekt/api/lib/config.ts
+++ IoT-dashboard - projekt/api/lib/config.ts
@@ -1,5 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const config = {
     port: process.env.PORT || 3100,
     supportedDevicesNum: 17,
-    databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://twwai:KTp5wYwutrLHPLT@cluster0.ooees.mongodb.net/IoT?retryWrites=true&w=majority'
- };
+    databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://twwai:KTp5wYwutrLHPLT@cluster0.ooees.mongodb.net/IoT?retryWrites=true&w=majority',
+    JwtSecret: "secret",
+    etherealEmail: process.env.ETHEREAL_EMAIL,
+    etherealPassword: process.env.ETHEREAL_PASSWORD
+};
